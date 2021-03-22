64d44057a7a31be3fecb40d460d911b656977256
clownchrys <clownchrys@gmail.com>
2021-03-22 18:56:30 +0900

    added utils.async.Loop

--- utils/async.py
+++ utils/async.py
@@ -1,8 +1,23 @@
 import asyncio
 
 
-class AsyncLoop:
-    """
-    implementation of contextmanager
-    """
-    pass
+class Loop:
+    _loop = {
+        'default': asyncio.get_event_loop,
+        'new': asyncio.new_event_loop,
+        'running': asyncio.get_running_loop
+    }
+    def __init__(self, kind: str, collapse: bool=False, set_event_loop: bool=False):
+        assert kind in self._loop, f"Invalid kind: {kind!r} (available {'|'.join(self._loop)})"
+
+        self.loop = self._loop[kind]
+        self.collapse = collapse
+        if set_event_loop:
+            asyncio.set_event_loop(self.loop)
+
+    def __enter__(self):
+        return self.loop
+
+    def __exit__(self, type, value, tb):
+        if self.collapse:
+            self.loop.close()
