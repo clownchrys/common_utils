ae61168d17b812edcf8c9bb4fbe098333f4c658a
clownchrys <clownchrys@gmail.com>
2021-03-22 19:00:29 +0900

    utils.aync.Loop: set_event_loop as default

--- utils/async.py
+++ utils/async.py
@@ -3,17 +3,16 @@ import asyncio
 
 class Loop:
     _loop = {
-        'default': asyncio.get_event_loop,
+        'current': asyncio.get_event_loop,
         'new': asyncio.new_event_loop,
         'running': asyncio.get_running_loop
     }
-    def __init__(self, kind: str, collapse: bool=False, set_event_loop: bool=False):
+    def __init__(self, kind: str, collapse: bool=False):
         assert kind in self._loop, f"Invalid kind: {kind!r} (available {'|'.join(self._loop)})"
 
         self.loop = self._loop[kind]
         self.collapse = collapse
-        if set_event_loop:
-            asyncio.set_event_loop(self.loop)
+        asyncio.set_event_loop(self.loop)
 
     def __enter__(self):
         return self.loop
