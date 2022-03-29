2938900defc2bbc13b3825760896c7d71e449162
clownchrys <47801617+clownchrys@users.noreply.github.com>
2022-03-29 10:48:12 +0900

    Update check.py
    added get_ip() method

--- utils/check.py
+++ utils/check.py
@@ -1,4 +1,5 @@
 from typing import Any
+import socket
 
 
 def is_exception(obj: Any) -> bool:
@@ -12,3 +13,17 @@ def is_iterable(obj: Any) -> bool:
         return False
     else:
         return True
+    
+
+def get_ip() -> str:
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    try:
+        s.connect(('10.255.255.255', 1))  # doesn't even have to be reachable
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = "127.0.0.1"
+    finally:
+        s.close()
+
+    return IP
