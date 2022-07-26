43eb0e426a9c34baf34b14f2c49ef964e924e52a
clownchrys <47801617+clownchrys@users.noreply.github.com>
2022-07-26 11:31:34 +0900

    Create sagemaker.py

---
+++ utils/sagemaker.py
@@ -0,0 +1,158 @@
+import time
+from warnings import warn
+from typing import List, Tuple, Callable
+from functools import wraps, partial, reduce
+from sagemaker.estimator import EstimatorBase
+# from airflow.models import Variable
+
+
+### Dummy Class (from airflow.models import Variable)
+class AirflowVariable(dict):
+    def set(self, k, v):
+        self[k] = v
+        warn("Dummy airflow variable using now...")
+Variable = AirflowVariable()
+### Dummy Class End
+
+
+def override(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs)
+    return wrapper
+
+
+class BaseWrapper:
+    def wrap(self, func: Callable, *args, **kwargs):
+        raise NotImplementedError("Subclass of BaseWrapper must implement wrap method")
+        
+    def __repr__(self):
+        class_name = self.__class__.__name__
+        parameters = ", ".join(f"{k}={v}" for k, v in self.__dict__.items())
+        return f"{class_name}({parameters})"
+
+
+class RetryWrapper(BaseWrapper):
+    def __init__(self, max_tries: int, wait: int or float, is_exponential: bool, exceptions: List[BaseException] or Tuple[BaseException]):
+        self.max_tries = max_tries
+        self.wait = wait
+        self.is_exponential = is_exponential
+        self.exceptions = exceptions
+        
+    @override
+    def wrap(self, func: Callable, *args, **kwargs):
+        # initialize
+        retry = 0
+        last_exception = None
+        
+        # run within loop
+        while True:
+            try:
+                return func(*args, **kwargs)
+            except Exception as e:
+                last_exception = e
+                
+                if type(e) in self.exceptions:
+                    retry += 1
+                else:
+                    retry = self.max_tries
+                    
+                if retry < self.max_tries:
+                    seconds = (self.wait ** retry) if self.is_exponential else self.wait
+                    time.sleep(seconds)
+                    print(f"... retrying ({retry})")
+                else:
+                    break
+        
+        # throw exception, when retry exceeded
+        raise last_exception
+
+
+class DynamicTrainWrapper(BaseWrapper):
+    def __init__(self, estimator: EstimatorBase, env_name: str, airflow_variable: str, dynamic_run_types: List[str]):
+        self.estimator = estimator
+        self.env_name = env_name
+        self.airflow_variable = airflow_variable
+        self.dynamic_run_types = dynamic_run_types
+        
+    @override
+    def wrap(self, func: Callable, *args, **kwargs):
+        # get current run type
+        last_run_index = self.dynamic_run_types.index(
+            Variable.get(self.airflow_variable, self.dynamic_run_types[-1])
+        )
+        current_run_index = (last_run_index + 1) % len(self.dynamic_run_types)
+        current_run_type = self.dynamic_run_types[current_run_index]
+        
+        # set environment
+        if self.estimator.__dict__.get("environment") is None:
+            self.estimator.environment = {}
+        self.estimator.environment[self.env_name] = current_run_type
+        
+        # run estimator
+        ret = func(*args, **kwargs)
+        
+        # save last run type
+        Variable.set(self.airflow_variable, current_run_type)
+        return ret
+
+
+class WrappedFunction:
+    def __init__(self, function: Callable):
+        """
+        Order of wrappers is important in some cases.
+        They are wrapping target function sequentially.
+        
+        Example 1)
+            `f.set_wrappers(retry_wrapper, some_wrapper)`
+        
+        This means `some_wrapper(retry_wrapper(target_function))`.
+        So, this cannot be run some_wrapper twice.
+        
+        Example 2)
+            `f.set_wrappers(some_wrapper, retry_wrapper)`
+        
+        This means `retry_wrapper(some_wrapper(target_function))`.
+        So, this can be run some_wrapper as times of retrying.
+        
+        In conclusion, I recommend you to use like this: `f.set_wrappers(inner_wrapper, outer_wrapper)`
+        """
+        self.function = function
+        self.wrappers = tuple()
+        
+    def __call__(self, *args, **kwargs):
+        _init = partial(self.function, *args, **kwargs)
+        _reducer = lambda func, wrapper: partial(wrapper.wrap, func=func)
+        return reduce(_reducer, self.wrappers, _init)()
+    
+    def __repr__(self):
+        indent = " " * 4
+        class_name = self.__class__.__name__
+        function_name = self.function.__func__.__qualname__
+        wrappers_desc = "".join(f"\n{indent * 2}{wrapper}," for wrapper in self.wrappers)
+        return f"{class_name}(\n{indent}fucntion={function_name},\n{indent}wrappers=[{wrappers_desc}\n{indent}]\n)"
+        
+    def set_wrappers(self, *wrappers: BaseWrapper):
+        self.wrappers = wrappers
+        
+        
+if __name__ == "__main__":
+    class CapacityError(Excpetion):
+        pass
+
+
+    class DummyEstimator():
+        def fit(self):
+            import random
+            if random.random() > 0.5:
+                raise CapacityError
+            else:
+                return 1
+
+    estimator = DummyEstimator()
+    wrapped_fit = WrappedFunction(estimator.fit)
+    wrapped_fit.set_wrappers(
+        RetryWrapper(max_tries=3, wait=1, is_exponential=False, exceptions=[CapacityError]),
+        DynamicTrainWrapper(estimator=estimator, env_name="TRAIN_TYPE", airflow_variable="TEST_FLAG", dynamic_run_types=["A", "B"]),
+    )
+    wrapped_fit()
